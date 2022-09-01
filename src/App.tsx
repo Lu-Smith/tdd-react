ea2b9d4c32a1935845bc18fa1d9cd9d0a89897fa
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-01 11:38:35 +0100

    Initial folders

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React, {FC } from 'react'
+
+const NavBar: FC = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar;
