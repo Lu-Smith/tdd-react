306e4fb3522dfe54ed5060bdb807c359ec65016c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-01 13:20:38 +0100

    Updated ContactModal.test.tsx

--- src/ContactModal/ContactModal.test.tsx
+++ src/ContactModal/ContactModal.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import ContactModal from './ContactModal';
 
-test('renders learn react link',async () => {
+afterEach(cleanup);
+
+test('Initializes empty form', () => {
   render(<ContactModal />);
-  const text = await screen.findByText('ContactModal');
-  expect(text).toBeInTheDocument();
+  const nameInput = screen.queryAllByPlaceholderText('Name');
+  const emailInput = screen.queryAllByPlaceholderText('Email');
+  const phoneInput = screen.queryAllByPlaceholderText('Phone');
+
+  expect(nameInput).toBeInTheDocument();
+  expect(emailInput).toBeInTheDocument();
+  expect(phoneInput).toBeInTheDocument();
+
+  expect(nameInput).toHaveValue('');
+  expect(emailInput).toHaveValue('');
+  expect(phoneInput).toHaveValue('');
 });
